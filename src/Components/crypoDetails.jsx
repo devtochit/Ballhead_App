cd9386407acb3f39c19681e53ab396887afd9eda
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-07-27 12:49:19 -0600

    feat: :+1:  crypodetails page

--- src/Components/crypoDetails.jsx
+++ src/Components/crypoDetails.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react'
+import { HTMLReactParser } from 'html-react-parser'
+import { useParams } from 'react-router-dom'
+import { Col, Row, Typography, Select } from 'antd'
+import Loader from './Loader'
+import LineChart from './lineChart'
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/api.service'
 
 
 function CrypoDetails() {
