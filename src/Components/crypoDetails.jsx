9bff99ed9c712718e698a9d52a99de0b57b13a4f
officialesse <uzomaesse@gmail.com>
2022-07-27 19:52:55 +0100

    Feat:ADDED ICON

--- src/Components/crypoDetails.jsx
+++ src/Components/crypoDetails.jsx
@@ -5,6 +5,7 @@ import { Col, Row, Typography, Select } from 'antd'
 import Loader from './Loader'
 import LineChart from './lineChart'
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/api.service'
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
 
 function CrypoDetails() {
