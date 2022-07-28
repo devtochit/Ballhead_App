6cad3cbec3dc9326868d8b6e67406a43177c013f
officialesse <uzomaesse@gmail.com>
2022-07-28 13:26:38 +0100

    feat: added nav

--- src/Components/navbar/homeNav.jsx
+++ src/Components/navbar/homeNav.jsx
@@ -1,27 +1,57 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../Styles/Header.css';
-import spaceLogo from '../images/space-logo.png';
+import React, { useState, useEffect } from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
-const Header = () => (
-  <nav>
-    <div className="header-section">
-      <div className="logo">
-        <img className="logo-img" src={spaceLogo} alt="logo" />
+import icon from '../images/cryptocurrency.png';
+
+const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo"><Link to="/">Cryptoverse</Link></Typography.Title>
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
       </div>
-      <ul className="myLinks">
-        <li>
-          <NavLink to="/" className="rockets">Rockets</NavLink>
-        </li>
-        <li>
-          <NavLink to="/mission" className="mission">Missions</NavLink>
-        </li>
-        <li>
-          <NavLink to="/profile" className="profile">My Profile</NavLink>
-        </li>
-      </ul>
+      {activeMenu && (
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+      )}
     </div>
-  </nav>
-);
+  );
+};
 
-export default Header;
+export default Navbar;
